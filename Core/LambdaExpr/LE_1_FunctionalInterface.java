07c78708010dc158f38a53864494261fa2eb8e8a
Kumar Gaurav <kmrgaurav11235@gmail.com>
2019-08-19 09:51:54 +0530

    Initial Commit

---
+++ Core/LambdaExpr/LE_1_FunctionalInterface.java
@@ -0,0 +1,34 @@
+// https://www.geeksforgeeks.org/functional-interfaces-java/
+@FunctionalInterface
+interface LE_1_Interface {
+  int getSquare(int num);
+  default void sayHello(String str) {
+    System.out.println("Hello " + str + ".");
+  }
+}
+public class LE_1_FunctionalInterface {
+  public static void main(String[] args) {
+    int testNum1 = 5, testNum2 = 7, testNum3 = 9, testNum4 = 12;
+
+    // Anonymous Class
+    LE_1_Interface f1 = new LE_1_Interface() {
+      @Override
+      public int getSquare(int num) {
+        return num * num;
+      }
+    };
+    System.out.println("Square of " + testNum1 + " is : " + f1.getSquare(testNum1));
+
+    // Lambda Expression
+    LE_1_Interface f2 = (int x) -> {return x * x;};
+    System.out.println("Square of " + testNum2 + " is : " + f2.getSquare(testNum2));
+
+    // Lambda Expression: dropping parameter type as it can be inferred
+    LE_1_Interface f3 = x -> {return x * x;};
+    System.out.println("Square of " + testNum3 + " is : " + f3.getSquare(testNum3));
+
+    // Lambda Expression: dropping return statement
+    LE_1_Interface f4 = x ->  x * x;
+    System.out.println("Square of " + testNum4 + " is : " + f4.getSquare(testNum4));
+  }
+}
---
+++ GFG.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    static class FastReader
+    {
+        BufferedReader br;
+        StringTokenizer st;
+        FastReader()
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next()
+        {
+            try
+            {
+                while(st == null || !st.hasMoreTokens())
+                    st = new StringTokenizer(br.readLine());
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+    }
+    static int recursiveBinarySearch(int arr[], int low, int high, int key) {
+        if (low <= high) {
+          int mid = (low + high) / 2;
+          if (key == arr[mid]) {
+            return mid;
+          } else if (key < arr[mid]) {
+            return recursiveBinarySearch(arr, low, mid - 1, key);
+          } else {
+            return recursiveBinarySearch(arr, mid + 1, high, key);
+          }
+        }
+        return -1;
+    }
+	public static void main (String[] args) {
+	    FastReader fr = new FastReader();
+	    int t = fr.nextInt(); // test cases
+	    int index; // index for for-loop of single test case
+	    int [] solution = new int[t]; // solution array
+
+	    for(index = 0; index < t; index++)
+	    {
+	        int n = fr.nextInt();
+	        int key = fr.nextInt();
+	        int [] a = new int [n];
+
+	        for(int i = 0; i < n; i++) {
+	            a[i] = fr.nextInt();
+	        } // Get the array
+
+	        solution[index] = recursiveBinarySearch(a, 0, n - 1, key);
+	    }
+
+	    for(index = 0; index < t; index++)
+	    {
+	        System.out.println((solution[index] == -1)?-1:1);
+	    }
+	}
+}
