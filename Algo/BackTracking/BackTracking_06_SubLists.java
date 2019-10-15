742b41a86a737ffd1e70b89a16aa70b5515e1a85
Kumar Gaurav <kmrgaurav11235@gmail.com>
2019-10-16 00:26:58 +0530

    Generics

---
+++ Algo/BackTracking/BackTracking_06_SubLists.java
@@ -0,0 +1,31 @@
+/*
+Write a program to print all possible sub-lists that you can make out of a given list.
+Lecture: https://www.youtube.com/watch?v=J_odcqzHGqw
+e.g.
+List = {Jane, Bob, Matt}
+All possible sub-lists:
+{}, {Jane}, {Bob}, {Matt}, {Jane, Bob}, {Jane, Matt}, {Bob, Matt}, {Jane, Bob, Matt}
+
+Note that the order is not important here.
+*/
+class BackTracking_06_SubLists {
+    /* 
+    Decision Tree:
+    The order of selection is not important here. A bad way to solve this problem is to solve it similar to previous
+    problem, i.e. to make the selection of all possible elements at one step:
+    1) Create two lists: selected = {} and remaining = {<All elements>}
+    2) For each of the elements in the list 'remaining':
+        - Select the element - remove it from 'remaining' and add it to 'selected'.
+        - Explore recursively.
+        - Backtrack - Remove it from 'selected' and add it to 'remaining'.
+    
+    But, this is a bad solution as it takes 'Order of elements' in account, e.g. it will print {Bob, Matt} and 
+    {Matt, Bob} seperately.
+
+    A better Decision tree will be based on whether an element is to be included or not:
+    
+    */
+    public static void main(String[] args) {
+        
+    }
+}
