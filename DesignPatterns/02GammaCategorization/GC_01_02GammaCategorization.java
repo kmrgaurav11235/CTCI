6e83b4eeec0357b54aa35ade63bdc6591105c5c7
Kumar Gaurav <kmrgaurav11235@gmail.com>
2019-10-17 10:18:32 +0530

    Gamma Categorization

---
+++ DesignPatterns/02GammaCategorization/GC_01_02GammaCategorization.java
@@ -0,0 +1,15 @@
+/*
+Gamma Categorization:
+Design patterna are typically split in three categories. This is called Gamma Categorization after
+Erich Gamma, one of the authors of the original Gang of four book : 'Design Patterns: Elements of 
+Reusable Object-Oriented Software (1994)'
+
+1. Creational Patterns: Deal with creation/contruction of Objects -- explicit (contructor) vs implicit
+    (Dependency Injection, reflection etc.), wholesale (single statement) vs piecewise (ste-by-step)
+    creation.
+2. Structural Patterns: Concerned with the structure (e.g. class members). Many patterns are wrappers
+    that mimic the underlying class interface. Stress the importance of Good API design.
+3. Behavioral Patterns: They are all different; there is no central theme.
+*/
+class GC_01_02GammaCategorization {
+}
---
+++ DesignPatterns/03Builder/Builder_01_JavaBuilder.java
@@ -0,0 +1,10 @@
+/*
+Having a massive object with 10 constructor arguments is not productive. Instead, a Builder allows
+for step-by-step construction.
+StringBuilder is Builder that is already a part of JDK.
+*/
+class Builder_01_JavaBuilder {
+    public static void main(String[] args) {
+        
+    }
+}
