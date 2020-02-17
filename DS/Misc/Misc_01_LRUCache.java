980dc15b90f40e9053449f68e3ccea4023bc2109
Kumar Gaurav <kmrgaurav11235@gmail.com>
2020-02-17 16:39:36 +0530

    LRU Cache

---
+++ DS/Misc/Misc_01_LRUCache.java
@@ -0,0 +1,69 @@
+// https://www.geeksforgeeks.org/lru-cache-implementation/
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class Misc_01_LRUCache {
+    static class LRUCache {
+        private int maxSize;
+        private HashSet<Integer> elements;
+        private Deque<Integer> queue;
+
+        LRUCache(int maxSize) {
+            this.maxSize = maxSize;
+            elements = new HashSet<>();
+            queue = new LinkedList<>();
+        }
+
+        public void display() {
+            System.out.println("LRU Cache: " + queue);
+        }
+
+        public void refer(int n) {
+            if (!elements.contains(n)) {
+                System.out.println("Cache Miss!");
+                if (queue.size() == maxSize) {
+                    // Remove Least frequently used
+                    int discarded = queue.pop();
+                    elements.remove(discarded);
+                }
+            } else {
+                System.out.println("Cache Hit!");
+                queue.remove(n);
+            }
+            // Add new element
+            queue.offer(n);
+            elements.add(n);            
+        }
+    }
+    public static void main(String[] args) {
+        LRUCache ca = new LRUCache(4); 
+        ca.display();
+
+        ca.refer(1); 
+        ca.display();
+
+        ca.refer(2); 
+        ca.display();
+
+        ca.refer(3); 
+        ca.display();
+
+        ca.refer(1); 
+        ca.display();
+
+        ca.refer(4); 
+        ca.display();
+        
+        ca.refer(5); 
+        ca.display(); 
+        
+        ca.refer(4); 
+        ca.display(); 
+        
+        ca.refer(1); 
+        ca.display(); 
+    }
+}
